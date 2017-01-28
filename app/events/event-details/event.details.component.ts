f05e1509be10138fa3d862954a859a190b9e7220
Sean <seon2you@hotmail.com>
2017-01-28 11:47:30 -0800

    routing

--- app/events/event-details/event.details.component.ts
+++ app/events/event-details/event.details.component.ts
@@ -3,7 +3,7 @@ import { EventService } from '../shared/event.service'
 import { ActivatedRoute } from '@angular/router'
 
 @Component({
-    templateUrl: '/app/events/event.details.component.html',
+    templateUrl: '/app/events/event-details/event.details.component.html',
     styles:[`
         .container { padding-left: 20px; padding-right: 20px; }
         .event-image { height: 100px; }

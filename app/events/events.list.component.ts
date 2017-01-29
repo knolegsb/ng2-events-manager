4495bb30cdc4f480be24a1800deb4083c1d67163
Sean <seon2you@hotmail.com>
2017-01-28 22:10:06 -0800

    changed all other event type to IEvent for typescript safe

--- app/events/events.list.component.ts
+++ app/events/events.list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 import { ToastrService } from '../common/toastr.service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/index'
 
 @Component({
     selector: 'events-list',
@@ -14,7 +15,7 @@ export class EventsListComponent implements OnInit {
         console.log('received:', data)
     }
 
-    events: any
+    events: IEvent[]
     constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute){
         //this.events = this.eventService.getEvents()
     }
